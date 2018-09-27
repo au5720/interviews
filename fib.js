5d669d56ae4efb35a9ce3ee2efc84e3fe057ecaf
jennifermorgan2016 <jennifer.morgan@ise.ie>
2018-09-27 12:11:50 +0100

    Small node to note the run time

--- fib.js
+++ fib.js
@@ -1,4 +1,6 @@
 
+
+
 const fib = (n) => {
   if(n < 2) {
     return 1;
@@ -6,6 +8,12 @@ const fib = (n) => {
   return fib(n-1) + fib(n-2);
 };
 
-console.log(fib(7));
-
-
+if(process.argv.length < 2) {
+  console.log('Usage: node fib.js 5');
+} else {
+  let n = process.argv[2];
+  let start=Date.now();
+  console.log(fib(n)); 
+  let end=Date.now(); 
+  console.log(`Run time :  + ${end-start} ms`);
+}
